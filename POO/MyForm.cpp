9bce7e5dbd4d42a1d06cc66a7f0e0d3546ab5e46
Pow3y <lukas.houille@viacesi.fr>
2022-11-29 10:31:55 +0100

    cout

--- POO/MyForm.cpp
+++ POO/MyForm.cpp
@@ -1,9 +1,9 @@
 #include "pch.h"
 #include "MyForm.h"
+#include <iostream>
 
 using namespace System;
 using namespace System::Windows::Forms;
-
 
 [STAThreadAttribute]
 int __clrcall WinMain(array<String^>^ args)
@@ -11,5 +11,8 @@ int __clrcall WinMain(array<String^>^ args)
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	POO::MyForm monFormulaire;
+	
+	std::cout << "Hello World!" << std::endl;
+	
 	Application::Run(% monFormulaire);
 }
