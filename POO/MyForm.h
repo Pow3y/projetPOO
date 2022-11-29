81d3caeb157d99952f47e097ec96428df4513ecc
Pow3y <lukas.houille@viacesi.fr>
2022-11-29 09:50:11 +0100

    small updates

--- POO/MyForm.h
+++ POO/MyForm.h
@@ -34,7 +34,7 @@ namespace POO {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^ button1;
+
 	protected:
 
 	private:
@@ -50,24 +50,13 @@ namespace POO {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
-			// 
-			// button1
-			// 
-			this->button1->Location = System::Drawing::Point(62, 120);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1575, 922);
-			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
--- POO/POO.cpp
+++ POO/POO.cpp
@@ -1,6 +1,4 @@
 #include "pch.h"
-#include "sql.h"
-
 
 using namespace System;
 
