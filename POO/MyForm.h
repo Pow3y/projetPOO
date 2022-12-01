851f083f7c2413fb77ef35565e772d6d6104d10f
Gabriel ANCIAUX <gabriel.anciaux@gmail.com>
2022-12-01 10:06:21 +0100

    design_2

--- POO/MyForm.h
+++ POO/MyForm.h
@@ -42,6 +42,14 @@ namespace POO {
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Panel^ panel2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button6;
+
+
+
+
 
 	protected:
 
@@ -64,34 +72,41 @@ namespace POO {
 			this->button4 = (gcnew System::Windows::Forms::Button());
 			this->button5 = (gcnew System::Windows::Forms::Button());
 			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->tableLayoutPanel1->SuspendLayout();
+			this->panel1->SuspendLayout();
+			this->panel2->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Anchor = System::Windows::Forms::AnchorStyles::None;
-			this->button1->Location = System::Drawing::Point(57, 161);
+			this->button1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->button1->Location = System::Drawing::Point(3, 3);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(167, 102);
+			this->button1->Size = System::Drawing::Size(223, 55);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"button1";
 			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// button2
 			// 
-			this->button2->Anchor = System::Windows::Forms::AnchorStyles::None;
-			this->button2->Location = System::Drawing::Point(498, 170);
+			this->button2->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->button2->Location = System::Drawing::Point(232, 3);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(167, 102);
+			this->button2->Size = System::Drawing::Size(223, 55);
 			this->button2->TabIndex = 1;
 			this->button2->Text = L"button2";
 			this->button2->UseVisualStyleBackColor = true;
 			// 
 			// button3
 			// 
-			this->button3->Anchor = System::Windows::Forms::AnchorStyles::None;
-			this->button3->Location = System::Drawing::Point(24, 311);
+			this->button3->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->button3->Location = System::Drawing::Point(461, 3);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(167, 102);
+			this->button3->Size = System::Drawing::Size(223, 55);
 			this->button3->TabIndex = 2;
 			this->button3->Text = L"button3";
 			this->button3->UseVisualStyleBackColor = true;
@@ -99,20 +114,20 @@ namespace POO {
 			// 
 			// button4
 			// 
-			this->button4->Anchor = System::Windows::Forms::AnchorStyles::None;
-			this->button4->Location = System::Drawing::Point(12, 419);
+			this->button4->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->button4->Location = System::Drawing::Point(690, 3);
 			this->button4->Name = L"button4";
-			this->button4->Size = System::Drawing::Size(167, 102);
+			this->button4->Size = System::Drawing::Size(223, 55);
 			this->button4->TabIndex = 3;
 			this->button4->Text = L"button4";
 			this->button4->UseVisualStyleBackColor = true;
 			// 
 			// button5
 			// 
-			this->button5->Anchor = System::Windows::Forms::AnchorStyles::None;
-			this->button5->Location = System::Drawing::Point(644, 361);
+			this->button5->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->button5->Location = System::Drawing::Point(919, 3);
 			this->button5->Name = L"button5";
-			this->button5->Size = System::Drawing::Size(167, 102);
+			this->button5->Size = System::Drawing::Size(223, 55);
 			this->button5->TabIndex = 4;
 			this->button5->Text = L"button5";
 			this->button5->UseVisualStyleBackColor = true;
@@ -130,33 +145,81 @@ namespace POO {
 				20)));
 			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
 				20)));
+			this->tableLayoutPanel1->Controls->Add(this->button1, 0, 0);
+			this->tableLayoutPanel1->Controls->Add(this->button4, 3, 0);
+			this->tableLayoutPanel1->Controls->Add(this->button5, 4, 0);
+			this->tableLayoutPanel1->Controls->Add(this->button3, 2, 0);
+			this->tableLayoutPanel1->Controls->Add(this->button2, 1, 0);
 			this->tableLayoutPanel1->Dock = System::Windows::Forms::DockStyle::Top;
 			this->tableLayoutPanel1->Location = System::Drawing::Point(0, 0);
 			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
 			this->tableLayoutPanel1->RowCount = 1;
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 100)));
-			this->tableLayoutPanel1->Size = System::Drawing::Size(1141, 122);
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 61)));
+			this->tableLayoutPanel1->Size = System::Drawing::Size(1145, 61);
 			this->tableLayoutPanel1->TabIndex = 5;
+			// 
+			// panel1
+			// 
+			this->panel1->Controls->Add(this->button6);
+			this->panel1->Dock = System::Windows::Forms::DockStyle::Top;
+			this->panel1->Location = System::Drawing::Point(0, 61);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(1145, 289);
+			this->panel1->TabIndex = 6;
+			// 
+			// button6
+			// 
+			this->button6->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->button6->Location = System::Drawing::Point(0, 0);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(1145, 289);
+			this->button6->TabIndex = 0;
+			this->button6->Text = L"button6";
+			this->button6->UseVisualStyleBackColor = true;
+			// 
+			// panel2
+			// 
+			this->panel2->Controls->Add(this->textBox1);
+			this->panel2->Dock = System::Windows::Forms::DockStyle::Bottom;
+			this->panel2->Location = System::Drawing::Point(0, 362);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(1145, 145);
+			this->panel2->TabIndex = 7;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->textBox1->Location = System::Drawing::Point(0, 0);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(1145, 22);
+			this->textBox1->TabIndex = 0;
+			this->textBox1->Text = L"dededezezd";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1141, 522);
+			this->ClientSize = System::Drawing::Size(1145, 507);
+			this->Controls->Add(this->panel2);
+			this->Controls->Add(this->panel1);
 			this->Controls->Add(this->tableLayoutPanel1);
-			this->Controls->Add(this->button5);
-			this->Controls->Add(this->button4);
-			this->Controls->Add(this->button1);
-			this->Controls->Add(this->button3);
-			this->Controls->Add(this->button2);
-			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->Margin = System::Windows::Forms::Padding(2);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
+			this->tableLayoutPanel1->ResumeLayout(false);
+			this->panel1->ResumeLayout(false);
+			this->panel2->ResumeLayout(false);
+			this->panel2->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
 	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
+private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+};
+private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
 };
 }
