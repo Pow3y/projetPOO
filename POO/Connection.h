89ee35cdeaa1fe34f28059c6056f0658b9e2dd72
Pow3y <lukas.houille@viacesi.fr>
2022-12-02 15:28:24 +0100

    test ingore

--- POO/Connection.h
+++ POO/Connection.h
@@ -3,5 +3,5 @@
 public ref class Login
 {
 public:
-	static System::String^ ConnectionString = L"Data Source = 45.2; Initial Catalog = POO; Integrated Security = True";
+	static System::String^ ConnectionString = L"Data Source = 45.2222; Initial Catalog = POO; Integrated Security = True";
 };
